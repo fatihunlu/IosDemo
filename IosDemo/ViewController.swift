8db4ef8faf7a5c27c47d15bd9e3d7c44f087e01f
Fatih Ünlü <unlu.fa@gmail.com>
2021-06-04 00:16:23 +0300

    calculator implemented

--- IosDemo/ViewController.swift
+++ IosDemo/ViewController.swift
@@ -9,11 +9,107 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainLabel: UILabel!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var btnMinus: UIButton!
+    @IBOutlet weak var btnMultiply: UIButton!
+    @IBOutlet weak var btnDivide: UIButton!
+    
+    
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var txtFirstNumber: UITextField!
+    @IBOutlet weak var txtSecondNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func Add(_ sender: Any) {
+        
+        var firstNumber: Int = 0;
+        var secondNumber: Int = 0;
+        if let result = Int(txtFirstNumber.text!) {
+            firstNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        if let result = Int(txtSecondNumber.text!) {
+            secondNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        
+        resultLbl.text = String(firstNumber + secondNumber);
+    }
+    
+    @IBAction func Minus(_ sender: Any) {
+        var firstNumber: Int = 0;
+        var secondNumber: Int = 0;
+        if let result = Int(txtFirstNumber.text!) {
+            firstNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        if let result = Int(txtSecondNumber.text!) {
+            secondNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        
+        resultLbl.text = String(firstNumber - secondNumber);
+    }
+    
+    @IBAction func Multiply(_ sender: Any) {
+        var firstNumber: Int = 0;
+        var secondNumber: Int = 0;
+        if let result = Int(txtFirstNumber.text!) {
+            firstNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        if let result = Int(txtSecondNumber.text!) {
+            secondNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        
+        resultLbl.text = String(firstNumber * secondNumber);
+    }
+    
+    @IBAction func Divide(_ sender: Any) {
+        var firstNumber: Int = 0;
+        var secondNumber: Int = 0;
+        if let result = Int(txtFirstNumber.text!) {
+            firstNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        if let result = Int(txtSecondNumber.text!) {
+            secondNumber = result;
+        } else {
+            resultLbl.text = String("please ..");
+            return;
+        }
+        
+        
+        resultLbl.text = String(firstNumber / secondNumber);
+    }
+    
+    
 }
 
